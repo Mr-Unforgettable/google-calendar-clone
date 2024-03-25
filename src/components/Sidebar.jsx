814d4958a2db5d30ab74280f9982f298e693aeef
Mr-Unforgettable <rulzabhinav69@gmail.com>
2024-03-25 20:56:44 +0530

    Added labels component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SmallCalender from "./SmallCalender";
 import CreateEventButton from "./CreateEventButton";
+import Labels from "./Labels";
 
 export default function Sidebar() {
   return (
     <div className="border p-5 w-64">
       <CreateEventButton />
       <SmallCalender />
+      <Labels />
     </div>
   );
 }
